b0013faa8db55a7edb29da837dc99e122c34216d
LoboGuardian <loboguardian.dev@gmail.com>
2024-11-24 18:42:15 -0500

    sixsix

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from gtts import gTTS
 
 nato_alphabet = {
+     #'1': 'One', '2': 'Two', '3': 'Three',
+     #'4': 'Four', '5': 'Five', '6': 'Six',
+     #'7': 'Seven', '8': 'Eight', '9': 'Nine',
+    '0': 'Zero',
     '1': 'One', '2': 'Two', '3': 'Three',
     '4': 'Four', '5': 'Five', '6': 'Six',
     '7': 'Seven', '8': 'Eight', '9': 'Nine',
-    '0': 'Zero',
 
     # Letters
     # 'A': ('Alfa', 'AHL fah'),
